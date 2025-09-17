46ac6cd8b746762344c99d98d2c061a7745f8662
Jorge Jimenez <jorge970102@outlook.com>
2025-09-17 11:06:17 -0500

    fix bugs in routes

--- src/config/auth0.config.ts
+++ src/config/auth0.config.ts
@@ -2,7 +2,7 @@ export const auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN || '',
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || '',
   authorizationParams: {
-    redirect_uri: window.location.origin + '/app/welcome',
+    redirect_uri: window.location.origin + '/app/home',
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,
     scope: 'openid profile email',
   },
